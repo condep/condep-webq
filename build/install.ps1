375133f05d29ebffcb34094e29106abb603878ef
Kjell Iver Breistøl <104171kjbr@Z63>
2017-01-09 21:51:33 +0100

    Added install script used for setting assebly version.

---
+++ build/install.ps1
@@ -0,0 +1,6 @@
+﻿#Setting version number in assemblies when building on appveyor
+$appVeyorBuildVersion = $env:APPVEYOR_BUILD_VERSION
+$version = $appVeyorBuildVersion | % {$_.replace("-beta",".")}
+Set-AppveyorBuildVariable -Name "assembly_version" -Value "$version"
+Set-AppveyorBuildVariable -Name "assembly_file_version" -Value "$version"
+Set-AppveyorBuildVariable -Name "assembly_informational_version" -Value "$version"
